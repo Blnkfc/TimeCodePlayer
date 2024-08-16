b1e6b84576e16d8e1f918002f24f427d21381c72
Blnkfc <pavlinus11v@gmail.com>
2024-08-16 17:23:18 +0300

    Added functionality for every main element of the tool(adding new timestamp, startin from timestamp on button press, stopping on button press, deleting timestamp, changing playback speed) Major style changes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'playButton': "url('https://cdn-icons-png.flaticon.com/512/153/153752.png')",
+        'stopButton': "url('https://cdn-icons-png.flaticon.com/512/5644/5644528.png')",
+        'deleteBtn': "url('https://cdn-icons-png.flaticon.com/512/7104/7104075.png')"
+      },
+      colors:{
+        custom:{
+          'scrollBg': "#280142",
+          'scrollThumb': "#ff5af9"
+        }
+      },
+      gridTemplateRows:{
+        'layoutActive': "auto auto 1fr",
+        'layoutPassive': "1fr auto 1fr"
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
 }
